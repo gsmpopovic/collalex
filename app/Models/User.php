338581b79dcec46614909aed9fcedf22b4c27243
George Popovic <georgesmpopovic@gmail.com>
2021-04-12 10:03:05 -0400

    LT.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Laratrust\Traits\LaratrustUserTrait;
 
 /**
  * Class User
@@ -25,6 +26,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class User extends Model
 {
+	use LaratrustUserTrait;
 	protected $table = 'users';
 
 	protected $dates = [
