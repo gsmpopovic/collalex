95de3976df197319ad7220a80f26d6669e67a9de
George Popovic <georgesmpopovic@gmail.com>
2021-04-13 21:23:54 -0400

    Working on public dictionary functionality

--- app/Http/Controllers/QueryPublicDictionaryController.php
+++ app/Http/Controllers/QueryPublicDictionaryController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Headword; 
 
 class QueryPublicDictionaryController extends Controller
 {
     //
+
+    public function index (Request $request){
+
+        $query_string = $request->q; 
+
+        $headwords = App\Models\Headword::where('headword', 'LIKE', '%'.$query_string.'%')->get();
+
+        return view('dictionary.template.show', ['headwords'=>$headwords]);
+    }
 }
