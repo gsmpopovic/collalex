7c2aeaacc7c87fd90410a77bd829e7bce3339c38
George Popovic <georgesmpopovic@gmail.com>
2021-06-28 00:28:47 -0400

    Dashboard welcome.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,7 +19,7 @@ class DashboardController extends Controller
 
     public function index(){
         
-        return view("dashboard.template.master");
+        return view("dashboard.template.welcome");
 
     }
 
---
+++ resources/views/dashboard/template/welcome.blade.php
@@ -0,0 +1,41 @@
+@extends('dashboard.template.master')
+@section('content')
+<header>
+    <div class="title">
+        <h2><a href="#">The Bantayanon Language Project</a></h2>
+    </div>
+    <div class="meta">
+    </div>
+</header>
+<div class="row">
+    <div class="6u 12u$(medium)">
+        <p><sup><i class="icon fa-quote-left"></i></sup> Maayong adlaw! Garbo nakon nga ipahibawo sa
+            inyo nga gihikay na sara ang usa ka diksyonaryo sa sinultian nga Bantayanon. Maayo gayud nga
+            makahibawo kita anhon pagswat, pagsulti sa paningog nga Binantayanon pinaagi sini nga
+            website. Dako namon nga ikahimuot ang inyo mga suhestyon kag panghunahuna. Tabangan naton
+            nga mapalambo ang sinultian nga Bantayanon.</p>
+        <p>Singgana kami kung nanu ang ara sa inyo mga hunahuna. Magpaabot kami. Hinaot nga inyo mapadya
+            ang inyo hunahuna bahin sini nga proyekto puhon.</p>
+        <p>Hinumduman naton ang mga gisulti sa aton bayani nga si Dr. Jose Rizal. Si Rizal nagsulti nga
+            ang wa kahibawo sa iya kaugalingon nga sinultihan mas bahoan pa sa lub-ok nga isda.</p>
+        <p>Hinaut makalingawan ninyo ang Bantayan Language Project! <sup><i
+                    class="icon fa-quote-right"></i></sup></p>
+    </div>
+
+    <div class="6u 12u$(medium)">
+        <p><sup><i class="icon fa-quote-left"></i></sup> <i>Welcome to the Bantayanon Language Project!
+                I am pleased to tell you that a Bantayanon Bisaya Language Project is now in the making
+                via this website. You'll get to know how Bantayanon words are written, spelled,
+                pronounced, spoken, its intonation and more!</i></p>
+        <p><i>We would appreciate your feedback. Let's help develop the Bantayanon language. Be a part
+                of the making of this dictionary and the development of MTB materials by sharing your
+                thoughts. We hope to hear from you soon.</i></p>
+        <p><i>Let us remember the words of our hero, Dr. Jose Rizal. Rizal said that he who does not
+                know his native language smells worse than rotten fish!</i></p>
+        <p><i>Have an enjoyable time with the Bantayanon Language Project!</i> <sup><i
+                    class="icon fa-quote-right"></i></sup></p>
+
+    </div>
+</div>
+    
+@endsection
