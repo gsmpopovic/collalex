bff9cb5b6cd7abe3040f3f3b5b7b228bda751371
George Popovic <georgesmpopovic@gmail.com>
2021-06-27 22:21:08 -0400

    Renamed _assets folder to ol_assets.

--- resources/views/dictionary/template/includes/meta.blade.php
+++ resources/views/dictionary/template/includes/meta.blade.php
@@ -1,10 +1,10 @@
 <title>Welcome | Bantayanon Language Development Project</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<!--[if lte IE 8]><script src="_assets/js/ie/html5shiv.js"></script><![endif]-->
+<!--[if lte IE 8]><script src="ol_assets/js/ie/html5shiv.js"></script><![endif]-->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<link rel="stylesheet" href="{{asset('_assets/css/main.css')}}" />
-<!--[if lte IE 9]><link rel="stylesheet" href="_assets/css/ie9.css" /><![endif]-->
-<!--[if lte IE 8]><link rel="stylesheet" href="_assets/css/ie8.css" /><![endif]-->
-<link rel="icon" href="asset(ol_assets/images/icon_beach_withB.ico')">
+<link rel="stylesheet" href="{{asset('ol_assets/css/main.css')}}" />
+<!--[if lte IE 9]><link rel="stylesheet" href="ol_assets/css/ie9.css" /><![endif]-->
+<!--[if lte IE 8]><link rel="stylesheet" href="ol_assets/css/ie8.css" /><![endif]-->
+<link rel="icon" href="asset(olol_assets/images/icon_beach_withB.ico')">
